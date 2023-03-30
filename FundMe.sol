0e41da97337dc65ffe8173d6fdda68c7cfffa4ae
Carefree-happy <2507057523@qq.com>
2023-03-30 17:07:19 +0800

    sol: withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -11,10 +11,43 @@ contract SafeMathTester {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
     function fund() public payable {
         // msg.value is consider as the first parameter for any of those library functions
-        require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough");
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+    
+    function withdraw() public onlyOwner {
+        
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+        // actually withdraw the funds
+
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        // do require then _ & do _ then require
+        require(msg.sender == owner, "Sender is not owner");
+        _;
     }
 }
