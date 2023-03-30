f819929b42843b508e107608a57295671b6c62bd
Carefree-happy <2507057523@qq.com>
2023-03-30 13:11:52 +0800

    sol: priceConverter

--- FundMe.sol
+++ FundMe.sol
@@ -1,35 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract SafeMathTester {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough");
+        // msg.value is consider as the first parameter for any of those library functions
+        require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        // Always multiple before you divide
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
     }
 }
